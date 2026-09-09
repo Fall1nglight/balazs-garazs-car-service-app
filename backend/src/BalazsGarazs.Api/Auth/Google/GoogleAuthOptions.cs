42e6f6d57e9bbd652a80e15adfe220117d7c4495
Fall1ngLight <kerulo.szabolcs.dev@gmail.com>
2026-09-09 22:18:56 +0200

    feat: add Google auth options

---
+++ backend/src/BalazsGarazs.Api/Auth/Google/GoogleAuthOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace BalazsGarazs.Api.Auth.Google;
+
+public class GoogleAuthOptions
+{
+    public const string SectionName = "Authentication:Google";
+    public required string ClientId { get; init; }
+    public required string ClientSecret { get; init; }
+}
